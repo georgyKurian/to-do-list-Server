ae11ed0503485a7ef58333140487628af0cca76c
Georgi Kurian <c0687988@mylambton.ca>
2018-01-07 17:40:51 -0500

    Added insert and other functionalities

--- src/routes.php
+++ src/routes.php
@@ -27,7 +27,7 @@ $app->get('/getdata', function (Request $request, Response $response, array $arg
       else{
         $r["is_complete"]  = true;
       }
-      
+
       array_push($list, $r);
     }
 
@@ -39,10 +39,87 @@ $app->get('/getdata', function (Request $request, Response $response, array $arg
 $app->get('/delete/{id}', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Delete data");
-    $id = $route->getArgument('id');
-    $handler = $this->db->query("SELECT * FROM items;");
+    $id = (int)$args['id'];
+
+    $handler = $this->db->prepare("DELETE FROM items WHERE id = :id;");
+    $handler->bindParam(':id', $id);
+    $handler->execute();
 
     $newResponse = $response->withHeader('Content-type', 'application/json');
-    $newResponse = $newResponse->withJson($list);
+    $newResponse = $newResponse->withJson(array("result" => "Deleted successfully"));
     return $newResponse;
 });
+
+$app->post('/update/{id}', function (Request $request, Response $response, array $args) {
+    $this->logger->info("Update data");
+    $id = (int)$args['id'];
+
+    $data = $request->getParsedBody();
+
+    //$jsonData = json_decode($data, true);
+
+    $handler = $this->db->prepare("UPDATE items SET heading = :heading, description = :description, is_complete = :is_complete, updated_at=NOW() WHERE id = :id;");
+    $handler->bindParam(':id', $id);
+    $handler->bindParam(':heading', $data["heading"]);
+    $handler->bindParam(':description', $data["description"]);
+    $handler->bindParam(':is_complete', convertBooleanToInt($data["is_complete"]));
+
+    if($handler->execute()){
+      $handler = $this->db->prepare("SELECT id, heading, description, is_complete FROM items WHERE id = :id;");
+      $handler->bindParam(':id', $id);
+      $handler->execute();
+
+      if($r = $handler->fetch(PDO::FETCH_ASSOC)){
+        $r["is_complete"] = convertIntToBoolean($r["is_complete"]);
+        $newResponse = $response->withHeader('Content-type', 'application/json');
+        $newResponse = $newResponse->withJson($r);
+        return $newResponse;
+      }
+    }
+    $newResponse = $response->withHeader('Content-type', 'application/json');
+    $newResponse = $newResponse->withJson(array("Error"=>"error in request"));
+    return $newResponse;
+});
+
+$app->post('/insert', function (Request $request, Response $response, array $args) {
+    $this->logger->info("Insert data");
+
+    $data = $request->getParsedBody();
+
+    $handler = $this->db->prepare("INSERT INTO items (heading , description, is_complete) VALUES(:heading, :description, :is_complete);");
+    $handler->bindParam(':heading', $data["heading"]);
+    $handler->bindParam(':description', $data["description"]);
+    $handler->bindParam(':is_complete', convertBooleanToInt($data["is_complete"]));
+
+    if($handler->execute()){
+      $id = $this->db->lastInsertId();
+
+      $handler = $this->db->prepare("SELECT id, heading, description, is_complete FROM items WHERE id = :id;");
+      $handler->bindParam(':id', $id);
+      $handler->execute();
+
+      if($r = $handler->fetch(PDO::FETCH_ASSOC)){
+        $r["is_complete"] = convertIntToBoolean($r["is_complete"]);
+        $newResponse = $response->withHeader('Content-type', 'application/json');
+        $newResponse = $newResponse->withJson($r);
+        return $newResponse;
+      }
+    }
+    $newResponse = $response->withHeader('Content-type', 'application/json');
+    $newResponse = $newResponse->withJson(array("Error"=>"error in data send"));
+    return $newResponse;
+});
+
+function convertIntToBoolean($data){
+  if($data == 0)
+    return false;
+  else
+    return true;
+}
+
+function convertBooleanToInt($data){
+  if($data == false)
+    return 0;
+  else
+    return 1;
+}
